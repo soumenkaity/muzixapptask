1d401d25acd32b490144bac0444b3de2e433f1bb
soumenkaity <soumenkaity97@gmail.com>
2019-10-16 11:57:24 +0530

    lombok added

--- src/main/java/com/stackroute/muzixapp/domain/Track.java
+++ src/main/java/com/stackroute/muzixapp/domain/Track.java
@@ -1,56 +1,22 @@
 package com.stackroute.muzixapp.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track {
 
     @Id
     int id;
     String name;
     String comment;
-
-    public Track() {
-
-    }
-
-    public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
 }
